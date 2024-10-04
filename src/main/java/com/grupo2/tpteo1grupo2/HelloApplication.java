e8f1d0a73c9573c2ac51f63ef1f8340ec6a131d5
Ulises Casal <casalulises@gmail.com>
2024-10-04 17:24:22 -0300

    Commit de Jar funcional

--- src/main/java/com/grupo2/tpteo1grupo2/HelloApplication.java
+++ src/main/java/com/grupo2/tpteo1grupo2/HelloApplication.java
@@ -7,6 +7,7 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
 
@@ -14,8 +15,8 @@ public class HelloApplication extends Application {
     public void start(Stage stage) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/grupo2/tpteo1grupo2/hello-view.fxml"));
-            Image icon = new Image(getClass().getResourceAsStream("/com/grupo2/tpteo1grupo2/icono.png"));
-            stage.getIcons().add(icon);
+            //Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/grupo2/tpteo1grupo2/icono.png")));
+            //stage.getIcons().add(icon);
             Scene scene = new Scene(fxmlLoader.load(), 600, 700);
             stage.setTitle("Compilador Grupo 2");
             stage.setScene(scene);
