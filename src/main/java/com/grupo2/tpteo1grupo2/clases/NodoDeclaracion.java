3d4a5dd96ef55cc7d13d9fe15bb6fe45d50ff315
Gonzalo <gu4n4rt@gmail.com>
2025-04-26 21:35:51 +0000

    avance DECLARE SECTION

---
+++ src/main/java/com/grupo2/tpteo1grupo2/clases/NodoDeclaracion.java
@@ -0,0 +1,7 @@
+package com.grupo2.tpteo1grupo2.clases;
+
+public class NodoDeclaracion extends Nodo{
+    public NodoDeclaracion(String nombre) {
+            super(nombre);
+        }
+}
---
+++ src/main/java/com/grupo2/tpteo1grupo2/clases/NodoTipo.java
@@ -0,0 +1,7 @@
+package com.grupo2.tpteo1grupo2.clases;
+
+public class NodoTipo extends Nodo{
+    public NodoTipo(String nombre) {
+        super(nombre);
+    }
+}
